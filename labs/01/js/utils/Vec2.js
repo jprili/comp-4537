a5b0a834a94fbb9072ddc94e8434b209d028d6dc
James Paolo Rili <85321518+jprili@users.noreply.github.com>
2026-09-12 15:40:38 -0700

    add documentation for Vec2

--- labs/01/js/utils/Vec2.js
+++ labs/01/js/utils/Vec2.js
@@ -1,10 +1,23 @@
 class Vec2 {
+    /**
+     * Create a random 2D vector.
+     * @param {number} lowX  - low x limit
+     * @param {number} highX - high x limit
+     * @param {number} lowY  - low y limit
+     * @param {number} highY - high y limit
+     * @returns 
+     */
     static randomVec = (lowX, highX, lowY, highY) => {
         const randomX = lowX + ((highX - lowX) * Math.random());
         const randomY = lowY + ((highY - lowY) * Math.random());
         return new Vec2(randomX, randomY);
     }
 
+    /**
+     * Instantiate a 2D vector.
+     * @param {number} x - x component
+     * @param {number} y - y component
+     */
     constructor(x, y) {
         this.x = x;
         this.y = y;
